932ed2cd64a3d8017624c9059c4383f4b94a627c
kaveh <kaveh1999nazari@gmail.com>
2025-04-23 15:54:09 +0330

    create models for favorite

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Favorite extends Model
 {
-    //
+    protected $table = 'favorites';
+
+    protected $fillable = [
+        'user_id',
+        'song_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function song(): BelongsTo
+    {
+        return $this->belongsTo(Song::class);
+    }
 }
