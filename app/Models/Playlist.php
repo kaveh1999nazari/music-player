cef2b3a58ec4898d831695b909f1fc4f566cfff9
kaveh <kaveh1999nazari@gmail.com>
2025-04-23 15:57:51 +0330

    create models for playlist

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Playlist extends Model
 {
-    //
+    protected $table = 'playlists';
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'is_public',
+        'share_token'
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
