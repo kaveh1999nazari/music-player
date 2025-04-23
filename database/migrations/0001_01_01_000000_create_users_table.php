7aaab4dfa11f7d4a343351509fe43092502653b4
kaveh <kaveh1999nazari@gmail.com>
2025-04-23 15:48:37 +0330

    create tables for songs and favorite and playlist

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('last_login')->nullable();
             $table->timestamps();
         });
 
---
+++ database/migrations/2025_04_23_120036_create_songs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artist');
+            $table->string('album')->nullable();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('share_token')->nullable();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->boolean('is_public')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
