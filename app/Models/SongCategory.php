2131dfe5cd4e88839c04d2380a4138fed2a15d8d
kaveh <kaveh1999nazari@gmail.com>
2025-04-26 10:04:22 +0330

    update Tables and Models

--- app/Models/SongCategory.php
+++ app/Models/SongCategory.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SongCategory extends Model
 {
-    //
+    protected $table = 'song_categories';
+
+    protected $fillable = [
+        'song_id',
+        'album_id'
+    ];
+
+    public function song(): BelongsTo
+    {
+        return $this->belongsTo(Song::class);
+    }
+
+    public function album(): BelongsTo
+    {
+        return $this->belongsTo(Album::class);
+    }
 }
