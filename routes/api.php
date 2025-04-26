9230adcc60c8027ba0432a355ee3d097256429b8
kaveh <kaveh1999nazari@gmail.com>
2025-04-26 11:05:05 +0330

    install api and jwt

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::post('/register', 'create');
+});
