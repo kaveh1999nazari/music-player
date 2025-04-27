384092fc16505217030f5e652f6e2184e0479e93
kaveh <kaveh1999nazari@gmail.com>
2025-04-27 08:40:06 +0330

    update middleware for users

--- app/Http/Middleware/UserAuthMiddleware.php
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -5,6 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class UserAuthMiddleware
 {
@@ -15,11 +19,22 @@ class UserAuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (! auth('api')->check()) {
-            return response()->json([
-                'message' => 'لطفا وارد شوید'
-            ], 401);
+        try {
+            // اینجا تلاش می‌کنیم توکن رو چک کنیم
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (! $user) {
+                return response()->json(['message' => 'لطفا وارد شوید.'], 401);
+            }
+
+        } catch (TokenExpiredException $e) {
+            return response()->json(['message' => 'اعتبار توکن شما به اتمام رسیده است، لطفا مجددا وارد شوید'], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['message' => 'لطفا مجددا وارد شوید'], 401);
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'ابتدا وارد پنل کاربری شوید'], 401);
         }
+
         return $next($request);
     }
 }
