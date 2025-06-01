50c0bf06a0185b606ca3d58b7b5181eeec3d2141
Kaveh <kaveh1999nazari@gmail.com>
2025-06-01 08:59:12 +0330

    fix dockerize

--- docker/bootstrap/entrypoint.sh
+++ docker/bootstrap/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-CRON_JOB="* * * * * cd /app && /usr/local/bin/php artisan schedule:run >> /app/storage/logs/schedule-debug.log 2>&1"
+CRON_JOB="* * * * * cd /music && /usr/local/bin/php artisan schedule:run >> /music/storage/logs/schedule-debug.log 2>&1"
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 
 # Start cron
 service cron start
 
+# Run migrations automatically
+cd /music
+php artisan migrate --force
+
 # run with supervisord
 exec "$@"
-
-
-
