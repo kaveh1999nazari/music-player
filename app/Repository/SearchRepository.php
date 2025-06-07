28340b0d9e09a8f8a362dd34d551e2b28d24c66d
k.nazari <kaveh1999nazari@gmail.com>
2025-06-07 09:52:13 +0330

    fix and update repository for meili-search

--- app/Repository/SearchRepository.php
+++ app/Repository/SearchRepository.php
@@ -11,30 +11,25 @@ class SearchRepository
 {
     public function searchSongs(string $query, int $perPage, int $page): \Illuminate\Pagination\LengthAwarePaginator
     {
-        return Song::query()
-            ->where('title', 'LIKE', "%$query%")
-            ->with('media')
-            ->paginate($perPage, ['*'], 'page', $page);
+        return Song::search($query)
+            ->paginate($perPage, 'page', $page);
     }
 
     public function searchAlbums(string $query, int $perPage, int $page): \Illuminate\Pagination\LengthAwarePaginator
     {
-        return Album::query()
-            ->where('name', 'LIKE', "%$query%")
-            ->paginate($perPage, ['*'], 'page', $page);
+        return Album::search($query)
+            ->paginate($perPage, 'page', $page);
     }
 
     public function searchArtists(string $query, int $perPage, int $page): \Illuminate\Pagination\LengthAwarePaginator
     {
-        return Artist::query()
-            ->where('name', 'LIKE', "%$query%")
-            ->paginate($perPage, ['*'], 'page', $page);
+        return Artist::search($query)
+            ->paginate($perPage, 'page', $page);
     }
 
     public function searchPlaylists(string $query, int $perPage, int $page): \Illuminate\Pagination\LengthAwarePaginator
     {
-        return Playlist::query()
-            ->where('title', 'LIKE', "%$query%")
-            ->paginate($perPage, ['*'], 'page', $page);
+        return Playlist::search($query)
+            ->paginate($perPage, 'page', $page);
     }
 }
